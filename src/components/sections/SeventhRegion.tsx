fe3b0ea10c08ef54f740ac0b3099c521095ac6cc
Sylvain-Lepoutre <lepoutre.sylvain@orange.fr>
2023-09-22 09:58:18 +0200

    feat: SeventhRegion

--- src/components/sections/SeventhRegion.tsx
+++ src/components/sections/SeventhRegion.tsx
@@ -9,16 +9,18 @@ export const SeventhRegion = () => {
   return (
     <Landmark
       aria-labelledby={id}
-      className="background m-8 rounded-2xl p-4 text-center text-white"
+      className="background m-8 rounded-2xl p-4 py-8 text-center text-white"
       landmarkRole="region"
     >
-      <p className="my-4 text-2xl">
-        Places limitées, réservez dès à présent vos places
-      </p>
+      <img alt="" className="mx-auto" src="logo_inolib2.png" />
+
       <Landmark.Heading className="my-4 text-3xl" id={id}>
-        Conférence online : <br /> L’ Accessibilité Numérique, un Monde
-        d’Opportunités
+        Conférence sur L’Accessibilité Numérique, un Monde d’Opportunités
       </Landmark.Heading>
+
+      <p className="my-4 text-2xl">
+        Plus d’une centaine d’entreprises ont déja fait leur réservation
+      </p>
       <CallToActionLink label="Réservez maintenant !" />
     </Landmark>
   );
