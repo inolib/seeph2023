082acf15218e7aafe09ccf8a64eda52bf80520d4
Matthieu Meignan <matthieu.meignan@inolib.com>
2023-10-10 01:27:49 +0200

    fix: linting error

--- src/helpers/toPhoneNumber.ts
+++ src/helpers/toPhoneNumber.ts
@@ -2,7 +2,7 @@ import { toCustom, type TransformOptions } from "@modular-forms/react";
 import { formatIncompletePhoneNumber } from "libphonenumber-js";
 
 export const toPhoneNumber = (options: TransformOptions) => {
-  return toCustom(
+  return toCustom<string>(
     (value) => formatIncompletePhoneNumber(new String(value).toString()),
     options,
   );
