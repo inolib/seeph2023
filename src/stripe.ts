5e34ec4f88136c5d7fdd0034b3126ecb78adf54d
Matthieu Meignan <matthieu.meignan@inolib.com>
2023-10-11 15:32:57 +0200

    fix: env variables

--- src/stripe.ts
+++ src/stripe.ts
@@ -1,6 +1,8 @@
 import { loadStripe, type StripeElementsOptions } from "@stripe/stripe-js";
 
-export const stripePromise = loadStripe(process.env.STRIPE_API_KEY ?? "");
+export const stripePromise = loadStripe(
+  import.meta.env.VITE_STRIPE_API_KEY ?? "",
+);
 
 export const getOptions = (clientSecret: string): StripeElementsOptions => ({
   appearance: {
