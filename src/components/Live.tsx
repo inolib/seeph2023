74f015377b2feb836f41e65e7fbb18fb0e25af85
Matthieu Meignan <matthieu.meignan@inolib.com>
2023-10-12 10:29:19 +0200

    fix: dot size

--- src/components/Live.tsx
+++ src/components/Live.tsx
@@ -8,7 +8,7 @@ export const Live = ({ children, className, ...rest }: Props) => {
   return (
     <div
       className={cn(
-        "flex items-center gap-[1ch] rounded-full border-[1px] border-white py-0.125 pl-0.5 pr-0.25 text-white after:h-[1rem] after:w-[1rem] after:rounded-full after:bg-turquoise",
+        "flex items-center gap-[1ch] rounded-full border-[1px] border-white py-0.125 pl-0.5 pr-0.25 text-white after:h-[1.375rem] after:w-[1.375rem] after:rounded-full after:bg-turquoise",
         className,
       )}
       {...rest}
